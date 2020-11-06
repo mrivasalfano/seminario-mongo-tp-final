96c0bc668c63676a7c1d21481143517e6c6dfeca
mrivasalfano <mrivasalfano@gmail.com>
2020-11-06 15:13:07 -0300

    Inicio de proyecto

---
+++ seminario-mongo-tp-final/src/main/java/com/tudai/bolivar/SeminarioMongoTpFinalApplication.java
@@ -0,0 +1,13 @@
+package com.tudai.bolivar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminarioMongoTpFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminarioMongoTpFinalApplication.class, args);
+	}
+
+}
---
+++ seminario-mongo-tp-final/src/test/java/com/tudai/bolivar/SeminarioMongoTpFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tudai.bolivar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarioMongoTpFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
